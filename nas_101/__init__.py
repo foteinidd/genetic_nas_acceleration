5691c82f267e58111aa32a5dbd4f037f78719895
Foteini Dervisi <foteiniderv@iti.gr>
2022-03-28 14:45:44 +0300

    nasbench101 skeleton scripts

---
+++ nas_101/__init__.py
@@ -0,0 +1,13 @@
+from .base_ops import *
+from .model import *
+from .model_spec import ModelSpec
+
+
+__all__ = ['ConvBnRelu',
+           'Conv1x1BnRelu',
+           'Conv3x3BnRelu',
+           'MaxPool3x3',
+           'Network',
+           'Cell',
+           'ModelSpec'
+           ]
