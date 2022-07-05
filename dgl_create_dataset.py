b37aa8aa3a5a82c1a88e39fb4bae8eed91999100
Foteini Dervisi <foteiniderv@gmail.com>
2022-07-06 00:48:52 +0300

    remove non-necessary dgl files

--- dgl_create_dataset.py
+++
@@ -1,7 +0,0 @@
-from dgl_dataset import NASBench101CellDataset
-from dgl_params import *
-import os
-
-
-os.makedirs('dgl_dataset/', exist_ok=True)
-dataset = NASBench101CellDataset(num_arch=NUM_ARCH, generate_data=True, import_data=False)
