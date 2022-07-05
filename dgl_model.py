c75293e798e96c2c20c35abd9da795722bdd022a
Foteini Dervisi <foteiniderv@gmail.com>
2022-07-05 10:55:49 +0300

    upload final dgl model and params

--- dgl_model.py
+++ dgl_model.py
@@ -1,45 +1,49 @@
 import torch.nn as nn
 import torch.nn.functional as F
 
-from dgl.nn import SAGEConv, MaxPooling
+from dgl.nn import SAGEConv, MaxPooling, SortPooling, GATv2Conv, GroupRevRes
 
 
 class GCN(nn.Module):
-    def __init__(self, in_feats, h_feats):
+    def __init__(self, in_feats, h_feats, dout_prob):
         super(GCN, self).__init__()
         self.sage_lstm_h_1 = SAGEConv(in_feats=in_feats, out_feats=h_feats, aggregator_type='lstm')
-        self.sage_lstm_h_2 = SAGEConv(in_feats=h_feats, out_feats=h_feats, aggregator_type='lstm')
+        self.sage_lstm_h_2 = SAGEConv(in_feats=h_feats, out_feats=h_feats//2, aggregator_type='lstm')
+        self.sage_lstm_h_3 = SAGEConv(in_feats=h_feats//2, out_feats=h_feats//4, aggregator_type='lstm')
+        self.sage_lstm_h_4 = SAGEConv(in_feats=h_feats//4, out_feats=h_feats//8, aggregator_type='lstm')
+        self.sage_lstm_h_5 = SAGEConv(in_feats=h_feats//8, out_feats=h_feats//16, aggregator_type='lstm')
+
+        # self.gatconv1 = GATv2Conv(in_feats=in_feats, out_feats=h_feats, num_heads=1, allow_zero_in_degree=True)
+        # self.gatconv2 = GATv2Conv(in_feats=h_feats, out_feats=h_feats, num_heads=1, allow_zero_in_degree=True)
         self.pool_1_m = MaxPooling()
-        self.fc1 = nn.Linear(in_features=h_feats, out_features=h_feats//2)
-        # self.fc2 = nn.Linear(in_features=h_feats//2, out_features=h_feats//4)
-        self.fc3 = nn.Linear(in_features=h_feats//2, out_features=1)
+        # self.pool_1_m = SortPooling(k=1)
+        self.fc1 = nn.Linear(in_features=h_feats//16, out_features=h_feats//32)
+        self.fc2 = nn.Linear(in_features=h_feats//32, out_features=1)
+        # self.dropout = nn.Dropout(p=dout_prob)
 
     def forward(self, g):
         with g.local_scope():
             x = g.ndata['layer'].float()
-            # print(x.shape)
             x = self.sage_lstm_h_1(g, x)
-            # print(x.shape)
-            # x = F.relu(x)
-            # print(x.shape)
-            x = self.sage_lstm_h_2(g, x)
-            # print(x.shape)
-            # x = F.relu(x)
-            # print(x.shape)
-            x = self.pool_1_m(g, x)
-            # print(x.shape)
-            x = x.view(x.size()[0], -1)
-            # print(x.shape)
-            x = self.fc1(x)
-            # print(x.shape)
             x = F.relu(x)
-            # print(x.shape)
-            # x = self.fc2(x)
-            # print(x.shape)
-            # x = F.relu(x)
-            # print(x.shape)
-            x = self.fc3(x)
-            # print(x.shape)
-            # x = F.softmax(x, dim=1)
-            # print(x.shape)
+            # x = self.dropout(x)
+            x = self.sage_lstm_h_2(g, x)
+            x = F.relu(x)
+            # x = self.dropout(x)
+            x = self.sage_lstm_h_3(g, x)
+            x = F.relu(x)
+            # x = self.dropout(x)
+            x = self.sage_lstm_h_4(g, x)
+            x = F.relu(x)
+            # x = self.dropout(x)
+            x = self.sage_lstm_h_5(g, x)
+            x = F.relu(x)
+            # x = self.dropout(x)
+            x = self.pool_1_m(g, x)
+            x = x.view(x.size()[0], -1)
+            x = self.fc1(x)
+            x = F.relu(x)
+            # x = self.dropout(x)
+            x = self.fc2(x)
+            x = F.relu(x)
             return x
--- dgl_params.py
+++ dgl_params.py
@@ -1,7 +1,15 @@
-NUM_ARCH = 10000
-TRAIN_PERCENTAGE = 0.8
+NUM_ARCH = 500
+TRAIN_PERCENTAGE = 0.3
 TRAIN_BATCH_SIZE = 64
 VAL_BATCH_SIZE = 64
 NUM_FILTERS = 150
 LEARNING_RATE = 0.001
+DROPOUT_PROBABILITY = 0
 NUM_EPOCHS = 100
+TRAIN_STATS_SAVE_DIR = 'dgl_model_train_stats'
+EVAL_STATS_SAVE_DIR = 'dgl_model_evaluation_stats'
+MODEL_NAME = 'gcn_{}filters_5sageconv_lstm_scaled_relu_trainperc{}_{}epochs_{}nasbench101archs'.format(NUM_FILTERS,
+                                                                                                       # int(DROPOUT_PROBABILITY*100),
+                                                                                                       int(TRAIN_PERCENTAGE * 100),
+                                                                                                       NUM_EPOCHS,
+                                                                                                       NUM_ARCH)
